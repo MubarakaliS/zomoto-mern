80d2350d0191320ca8fde351b03716e4931c6ff3
MubarakaliS <145560592+MubarakaliS@users.noreply.github.com>
2024-05-09 18:37:37 +0530

    Update QuickSearch.js

--- zomoto/src/Components/QuickSearch.js
+++ zomoto/src/Components/QuickSearch.js
@@ -1,82 +1,19 @@
-import React, { useEffect, useState } from "react";
-import "../style/HomeStyle.css";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-const QuickSearch = () => {
-  const [mealData, setMealData] = useState([]);
-  const navigate = useNavigate();
-  const getMealData = () => {
-    axios
-      .get(`https://backend-api-rlne.onrender.com/getallmeal`)
-      .then((response) => response.data)
-      .then((data) => {
-        setMealData(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching restaurant details:", error);
-      });
-  };
-  const selectingMeal = (item) => {
-    sessionStorage.setItem('mealType',item.name)
-    const locationId = sessionStorage.getItem("locationId");
-    if (locationId) {
-      navigate(`/filter?mealTypeId=${item.meal_type}&locationId=${locationId}`);
-    } else {
-      navigate(`/filter?mealTypeId=${item.meal_type}`);
-    }
-  };
-  useEffect(() => {
-    getMealData();
-  }, []);
-  const imag = "../Assets/dinner.jpg";
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Home";
+import Filter from "./Filter";
+import TabsForm from "./Tabs";
+import Header from "./Header";
+const Router = () => {
   return (
-    <>
-      <div className="container">
-        <h1 className="display-6">Quick Searches</h1>
-        <span className="text-muted">Discover Restaurent by type of meal</span>
-        <div className="row mb-5 row-col-md-2 row-col-sm-3">
-          {mealData.map((item, index) => {
-            const name = item.image;
-            console.log(typeof name);
-            const pic1 = `/${item.image}`;
-            console.log(pic1);
-            return (
-              <div
-                className="col-10 col-lg-4 col-md-6 col-sm-10"
-                key={item._id}
-                style={{ cursor: "pointer" }}
-                onClick={() => selectingMeal(item)}
-              >
-                <div className="foodBox justify-content-center text-wrap">
-                  <img
-                    src={item.image}
-                    alt={`Thumbnail of ${item.name}`}
-                  />
-                  <h1
-                    style={{ fontSize: "2.0em", justifyContent: "left" }}
-                    className="p-2 mx-2"
-                  >
-                    {item.name}
-                  </h1>
-                  <h3
-                    style={{
-                      fontSize: "0.8em",
-                      justifyContent: "left",
-                      marginLeft: "5px",
-                    }}
-                    className="p-2 mr-2"
-                  >
-                    {item.content}
-                  </h3>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route path="/filter" Component={Filter} />
+        <Route path="/detail" Component={TabsForm} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
-export default QuickSearch;
+export default Router;
