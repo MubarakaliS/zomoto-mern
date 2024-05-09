0f0cd8d1772d4cb00b133c686fb400ddeffd98b7
MubarakaliS <145560592+MubarakaliS@users.noreply.github.com>
2024-05-09 17:54:13 +0530

    Update Filter.js

--- zomoto/src/Components/Filter.js
+++ zomoto/src/Components/Filter.js
@@ -27,7 +27,7 @@ const Filter = () => {
   const mealItem = () => {
     axios({
       method: "POST",
-      url: `http://localhost:4000/filter`,
+      url: `https://backend-api-rlne.onrender.com/filter`,
       headers: { "Content-Type": "application/json" },
       data: filterObj,
     })
@@ -43,7 +43,7 @@ const Filter = () => {
   const [locationData, setLocationData] = useState([]);
   const getEvents = () => {
     axios
-      .get("http://localhost:4000/getallloc")
+      .get("https://backend-api-rlne.onrender.com/getallloc")
       .then((response) => response.data)
       .then((data) => {
         setLocationData(data);
@@ -69,7 +69,7 @@ const Filter = () => {
     };
     axios({
       method: "POST",
-      url: `http://localhost:4000/filter`,
+      url: `https://backend-api-rlne.onrender.com/filter`,
       headers: { "Content-Type": "application/json" },
       data: filterObj,
     })
@@ -102,7 +102,7 @@ const Filter = () => {
     };
     axios({
       method: "POST",
-      url: `http://localhost:4000/filter`,
+      url: `https://backend-api-rlne.onrender.com/filter`,
       headers: { "Content-Type": "application/json" },
       data: filterObj,
     })
@@ -130,7 +130,7 @@ const Filter = () => {
     };
     axios({
       method: "POST",
-      url: `http://localhost:4000/filter`,
+      url: `https://backend-api-rlne.onrender.com/filter`,
       headers: { "Content-Type": "application/json" },
       data: filterObj,
     })
@@ -157,7 +157,7 @@ const Filter = () => {
     };
     axios({
       method: "POST",
-      url: `http://localhost:4000/filter`,
+      url: `https://backend-api-rlne.onrender.com/filter`,
       headers: { "Content-Type": "application/json" },
       data: filterObj,
     })
