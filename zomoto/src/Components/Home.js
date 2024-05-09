e08e2f9cdd7486c9b7253c41d38ce930129b9518
MubarakaliS <145560592+MubarakaliS@users.noreply.github.com>
2024-05-09 18:12:41 +0530

    Update Home.js

--- zomoto/src/Components/Home.js
+++ zomoto/src/Components/Home.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Wallpaper from "./Wallpaper";
 import QuickSearch from "./QuickSearch";
-import Carousels from "./Carousel";
-
-import TabsForm from "./Tabs";
-import Header from "./Header";
-import Wallpaper2 from "./Wallpaper2";
 const Home = () => {
   sessionStorage.clear();
   return (
